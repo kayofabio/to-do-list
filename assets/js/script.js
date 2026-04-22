e42f0e4747c8d1a4c69b986739244d882958a70a
kayofabio <kayofabio.dev@gmail.com>
2026-04-22 10:51:51 -0300

    add, remove and clear tasks function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,34 +1,108 @@
-document.querySelectorAll(".item").forEach(item => {
-    item.addEventListener("dragstart", dragStart);
-    item.addEventListener("dragend", dragEnd);
+// add item
+
+document.querySelectorAll(".add-item-btn").forEach(btn => {
+    btn.addEventListener("click", () => {
+        let categoryInput = btn.getAttribute("data-input");
+        let input = document.getElementById(categoryInput);
+
+        input.showModal();
+    });
 });
 
-function dragStart(e) {
-    e.currentTarget.classList.add("dragging");
-}
+document.querySelectorAll(".submit-item").forEach(btn => {
+    btn.addEventListener("click", () => {
+        let categoryList = btn.getAttribute("data-category-list");
+        let taskInput = btn.getAttribute("data-task")
 
-function dragEnd(e) {
-    e.currentTarget.classList.remove("dragging");
-}
+        let task = document.getElementById(taskInput);
+        let list = document.getElementById(categoryList);
+        
+        let li = document.createElement("li");
+        li.classList.add("item");
+        li.draggable = true;
+        li.textContent = task.value;
+        list.appendChild(li);
+        task.value = ""
 
-document.querySelectorAll(".area-task").forEach(area => {
-    area.addEventListener("dragover", dragOver);
-    area.addEventListener("dragleave", dragLeave);
-    area.addEventListener("drop", drop);
+        let categoryInput = btn.getAttribute("data-input");
+        let input = document.getElementById(categoryInput);
+        input.close();
+
+        dragAndDropSystem()
+    });
 });
 
-function dragOver(e) {
-    e.preventDefault();
+//remove item
+
+document.querySelector(".remove-item").addEventListener("dragover", dragOverRemove);
+document.querySelector(".remove-item").addEventListener("dragleave", dragLeaveRemove);
+document.querySelector(".remove-item").addEventListener("drop", dropRemove);
+
+function dragOverRemove(e) {
     e.currentTarget.classList.add("hover");
+    e.preventDefault();
 }
 
-function dragLeave(e) {
+function dragLeaveRemove(e) {
     e.currentTarget.classList.remove("hover");
+
 }
 
-function drop(e) {
+function dropRemove(e) {
     e.currentTarget.classList.remove("hover");
     let dragItem = document.querySelector(".dragging");
-    let taskList = e.currentTarget.querySelector("ul");
-    taskList.appendChild(dragItem);
+    dragItem.remove();
 }
+
+// clear itens
+
+document.querySelectorAll(".clear-itens-btn").forEach(btn => {
+    btn.addEventListener("click", () => {
+        let categoryList = btn.getAttribute("data-category-list");
+        let list = document.getElementById(categoryList);
+        list.replaceChildren();
+    })
+});
+
+// drag and drop
+let dragAndDropSystem = () => {
+
+    document.querySelectorAll(".item").forEach(item => {
+        item.addEventListener("dragstart", dragStart);
+        item.addEventListener("dragend", dragEnd);
+    });
+
+    function dragStart(e) {
+        e.currentTarget.classList.add("dragging");
+        document.querySelector(".remove-item").style.visibility = "visible";
+    }
+
+    function dragEnd(e) {
+        e.currentTarget.classList.remove("dragging");
+        document.querySelector(".remove-item").style.visibility = "hidden";
+    }
+
+    document.querySelectorAll(".area-itens").forEach(area => {
+        area.addEventListener("dragover", dragOver);
+        area.addEventListener("dragleave", dragLeave);
+        area.addEventListener("drop", drop);
+    });
+
+    function dragOver(e) {
+        e.preventDefault();
+        e.currentTarget.classList.add("hover");
+    }
+
+    function dragLeave(e) {
+        e.currentTarget.classList.remove("hover");
+    }
+
+    function drop(e) {
+        e.currentTarget.classList.remove("hover");
+        let dragItem = document.querySelector(".dragging");
+        let itemList = e.currentTarget.querySelector("ul");
+        itemList.appendChild(dragItem);
+    }
+}
+
+dragAndDropSystem();
